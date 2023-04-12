e975feece03f81bced5744631c8cdb36daef1853
Pedro Quintas <pedroquintas1213@gmail.com>
2023-04-11 22:04:30 -0300

    Projeto_Silvia

---
+++ FACULDADE_FRONT/projeto_aula_silvia/Quiz.js
@@ -0,0 +1,9 @@
+function continarPagina() {
+   
+    alert("Obrigado por responder o quiz!");
+    window.location.href="Projeto.html"
+  }
+
+function restaurarPagina(){
+    location.reload();
+}
--- FACULDADE_FRONT/projeto_aula_silvia/Registro.js
+++ FACULDADE_FRONT/projeto_aula_silvia/Registro.js
@@ -1,4 +1,30 @@
 function validarFormulario() {
+
+  var dataNascimento = document.getElementById("dataNascimento").value;
+  var dataAtual = new Date();
+  var anoAtual = dataAtual.getFullYear();
+  var mesAtual = dataAtual.getMonth() + 1;
+  var diaAtual = dataAtual.getDate();
+
+  var anoNascimento = parseInt(dataNascimento.split("-")[0]);
+  var mesNascimento = parseInt(dataNascimento.split("-")[1]);
+  var diaNascimento = parseInt(dataNascimento.split("-")[2]);
+
+  var idade = anoAtual - anoNascimento;
+
+  if (mesAtual < mesNascimento || (mesAtual === mesNascimento && diaAtual < diaNascimento)) {
+      idade--;
+  }
+
+  if (idade >= 18) {
+      alert("Você é maior de idade.");
+  } else {
+      alert("Você é menor de idade.");
+      location.reload();
+      return false    
+    }
+
+
   var caixas = document.getElementsByClassName("input");
 
   for (var i = 0; i < caixas.length; i++) {
@@ -12,14 +38,6 @@ if(caixas!= ""){
   location.reload();
   alert("Voce foi resgistrado!")
   window.location.href="quiz.html"
-
-}
-
-var idade = document.getElementsByClassName("input")
-
-if  (idade<18){
-
-  alert("Você é menor de idade")
 
 }
 
